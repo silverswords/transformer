34bbdfdf7518cfac185cadbf2bc804a0200286fe
Feng Yifei <fengyfei@gmail.com>
2021-08-31 11:07:42 +0800

    exception handling

--- index.js
+++ index.js
@@ -8,11 +8,17 @@ if (argv.length < 3) {
 
 async function main() {
     for (let i = 2; i < argv.length; i++) {
-        let executor = async function () {
-            await require(`./${argv[i]}/index.js`).executor()
-        }
+        const site = argv[i]
 
-        await executor()
+        try {
+            let executor = async function () {
+                await require(`./${site}/index.js`).executor()
+            }
+
+            await executor()
+        } catch (err) {
+            console.log(`${site} - ${err}`)
+        }
     }
 }
 
