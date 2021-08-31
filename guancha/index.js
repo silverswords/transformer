f6f4db8ef9a3d9bc4dacadee1b776072eebe33ae
Feng Yifei <fengyfei@gmail.com>
2021-08-31 15:29:00 +0800

    guancha: hot keys

--- guancha/index.js
+++ guancha/index.js
@@ -1,9 +1,46 @@
-const delay = require('../libs/delay.js').delay
+const puppeteer = require('puppeteer')
+
+const { setUserAgent, setViewportLarge } = require('../libs/agent.js')
+const { setupPageEvents } = require('../libs/page_events.js')
 
 async function executor() {
-    console.log('start crawling guancha.com')
+    const browser = await puppeteer.launch()
 
-    await delay(3000)
+    const page = await browser.newPage()
+    await setUserAgent(browser, page)
+    await setViewportLarge(page)
+
+    setupPageEvents(page, 'GUANCHA')
+
+    await page.goto('https://www.guancha.cn/', {
+        waitUntil: 'networkidle2',
+    });
+
+    const hotKeys = await page.$eval('.module-hot-point', (node) => {
+        const children = node.children
+
+        let rets = []
+        for (let i = 0; i < children.length; i++) {
+            const item = children[i]
+            if (item.tagName != 'DIV') {
+                continue
+            }
+
+            const a = item.children[0]
+            const span = a.children[0]
+            
+            rets.push({
+                title: span.innerHTML,
+                link: a.href
+            })
+        }
+
+        return rets
+    })
+    console.log(hotKeys)
+
+    await page.close()
+    await browser.close();
 }
 
 module.exports = {
